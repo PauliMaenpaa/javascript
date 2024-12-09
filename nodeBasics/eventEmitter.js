434d7e82fc178467d228389aa7713cd54c5109f4
Pauli Mäenpää <maenpaapauli@gmail.com>
2024-12-09 20:20:35 +0200

    /nodeBasics/eventEmitter.js

---
+++ nodeBasics/eventEmitter.js
@@ -0,0 +1,27 @@
+// import built in events module
+
+const EventEmitter = require("events");
+
+// create eventEmitter object from EventEmitter class
+
+const eventEmitter = new EventEmitter();
+
+// includes on and emit methods + many others
+
+// use on method to create events. Parameters are event name and a callback function, that will be executed when event is trigged
+
+eventEmitter.on("start", () => {
+  console.log("started");
+});
+
+// use emit method, to trigger a specific event
+
+eventEmitter.emit("start");
+
+// its possible to pass args to event handler by passing them as additional args to emit method
+
+eventEmitter.on("start_args", (num1, num2) => {
+  console.log(`started, args: ${num1}, ${num2}`);
+});
+
+eventEmitter.emit("start_args", 1, 2);
